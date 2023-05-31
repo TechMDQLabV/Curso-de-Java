4e9e60c0674b9a30aec8d815acf4a4f625d1dbc8
Sergio Garguir <sergiogarguir@gmail.com>
2023-05-31 10:32:46 -0300

    comparativa - ciclos

--- clase_optional/src/org/sergiogarguir/optional/ejemplo/ControlDeEficiencia.java
+++ clase_optional/src/org/sergiogarguir/optional/ejemplo/ControlDeEficiencia.java
@@ -13,7 +13,11 @@ import java.util.logging.Logger;
 public class ControlDeEficiencia {
 
     private static Logger LOGGER = Logger.getLogger("ControlDeEficiencia");
-    public static int CANTIDAD = 100000;
+    public static int CANTIDAD = 1900000;
+
+    public static String DOUBLE_LINE = "========================================================================================================================================================================";
+
+    public static String SIMPLE_LINE = "------------------------------------------------------------------------------------";
 
     public static void main(String[] args) {
         //List<Person> persons = buildPersons(CANTIDAD);
@@ -21,46 +25,78 @@ public class ControlDeEficiencia {
         //persons.forEach(System.out::println);
         LOGGER.info("titulo");
 
-        for (int i = 0; i < 10; i++){
-            List<Person> persons = null;
-            persons = buildPersons(CANTIDAD);
-            System.out.println("------------------------------------------------------------------------------------");
-            System.out.println("------------------------------------------------------------------------------------");
+        String name;
+        for (int i = 0; i < 1; i++){
+            System.out.println(SIMPLE_LINE);
+            System.out.println(SIMPLE_LINE);
             System.out.println("Iteration " + (i + 1));
-            System.out.println("------------------------------------------------------------------------------------");
-            System.out.println("------------------------------------------------------------------------------------");
+            System.out.println(SIMPLE_LINE);
+            System.out.println(SIMPLE_LINE);
 
             // select a random number for concat with name to search
             int randomNumber = (int)(Math. random()*CANTIDAD+1);
-
+            //name = personsFor.get(personsFor.size()-1).getName();
+            name = "Pepe " + (CANTIDAD - 1);
             System.out.println("Searching: \"Pepe "+randomNumber+"\"");
 
-            // Run find method with bucle
-            long startTimeHS = System.currentTimeMillis();
-            Person personFor = findName(persons, "Pepe " + randomNumber);
-            if (personFor!=null) {
-                System.out.print("Find with bucle in ");
+            // Run find method with While
+            List<Person> personsWhile = buildPersons(CANTIDAD);
+            long startTimeHSWhile = System.currentTimeMillis();
+            Person personWhile = findNameWhile(personsWhile, name);
+            if (personWhile!=null) {
+                System.out.print("Find with While in ");
             } else {
-                System.out.print("Not found with bucle in ");
+                System.out.print("Not found with While in ");
+            }
+            System.out.println(System.currentTimeMillis() - startTimeHSWhile + "ms");
+            System.out.println(personWhile);
+            System.out.println(DOUBLE_LINE);
+
+            // Run find method with for
+            List<Person> personsFor = buildPersons(CANTIDAD);
+            long startTimeHS = System.currentTimeMillis();
+            Person personFor = findNameFor(personsFor, name);
+            if (personFor!=null) {
+                System.out.print("Find with For in ");
+            } else {
+                System.out.print("Not found with For in ");
             }
             System.out.println(System.currentTimeMillis() - startTimeHS + "ms");
             System.out.println(personFor);
-            System.out.println("====================================================================================");
+            System.out.println(DOUBLE_LINE);
+
+
+            // Run find method with enhanced for
+            List<Person> personsEnhancedFor = buildPersons(CANTIDAD);
+            long startTimeHSEnhancedFor = System.currentTimeMillis();
+            Person personEnhancedFor = findNameEnhancedFor(personsEnhancedFor, name);
+            if (personEnhancedFor!=null) {
+                System.out.print("Find with Enhanced For in ");
+            } else {
+                System.out.print("Not found with Enhanced For in ");
+            }
+            System.out.println(System.currentTimeMillis() - startTimeHSEnhancedFor + "ms");
+            System.out.println(personFor);
+            System.out.println(DOUBLE_LINE);
 
             // Run find method with lamda anyMatch
+            List<Person> personsLamdaAnyMatch = buildPersons(CANTIDAD);
+            //name = personsLamdaAnyMatch.get(personsLamdaAnyMatch.size()-1).getName();
             long startTimeHSLamdaAnyMatch = System.currentTimeMillis();
-            boolean foundIt = findNameWithLambdaAnyMatch(persons, "Pepe " + randomNumber);
+            boolean foundIt = findNameWithLambdaAnyMatch(personsLamdaAnyMatch, name);
             if (foundIt) {
                 System.out.print("Find with lamdaAnyMatch in ");
             } else {
                 System.out.print("Not found with lamdaAnyMatch in ");
             }
             System.out.println(System.currentTimeMillis() - startTimeHSLamdaAnyMatch + "ms");
-            System.out.println("====================================================================================");
+            System.out.println(DOUBLE_LINE);
 
             // Run find method with lamda
+            List<Person> personsLamda = buildPersons(CANTIDAD);
+            //name = personsLamda.get(personsLamda.size()-1).getName();
             long startTimeHSLamda = System.currentTimeMillis();
-            Person personStream = findNameWithLambda(persons, "Pepe " + randomNumber);
+            Person personStream = findNameWithLambda(personsLamda, name);
             if (personStream!=null) {
                 System.out.print("Find with lamda in ");
             } else {
@@ -68,7 +104,7 @@ public class ControlDeEficiencia {
             }
             System.out.println(System.currentTimeMillis() - startTimeHSLamda + "ms");
             System.out.println(personStream);
-            System.out.println("====================================================================================");
+            System.out.println(DOUBLE_LINE);
         }
 
     }
@@ -81,13 +117,36 @@ public class ControlDeEficiencia {
         return false;
     }
 */
-    public static Person findName(List<Person> people, String name) {
-        Person p = null;
+
+public static Person findNameWhile(List<Person> people, String name){
+    Person p;
+    int i=0;
+    while(i < people.size()){
+        p = people.get(i);
+        if(p.getName().equals(name)){
+            return p;
+        }
+        i++;
+    }
+    return null;
+}
+
+public static Person findNameFor(List<Person> people, String name) {
+    Person p;
+    for(int i=0; i < people.size();i++) {
+        p = people.get(i);
+        if (p.getName().equals(name))
+            return p;
+    }
+    return null;
+}
+
+    public static Person findNameEnhancedFor(List<Person> people, String name) {
         for (Person person : people)
             if (person.getName().equals(name))
                 return person;
 
-        return p;
+        return null;
     }
 
     public static boolean findNameWithLambdaAnyMatch(List<Person> people, String name) {
